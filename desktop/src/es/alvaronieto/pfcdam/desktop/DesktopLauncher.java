49cfa703dc48015f652e4f0f332f6302d87da1bd
Álvaro <alvaronietofernandez@gmail.com>
2017-04-02 22:17:57 +0200

    Initial commit

---
+++ desktop/src/es/alvaronieto/pfcdam/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package es.alvaronieto.pfcdam.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import es.alvaronieto.pfcdam.Juego;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Juego(), config);
+	}
+}
